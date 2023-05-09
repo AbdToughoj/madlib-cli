7ba721ae634c3c492706d8d87df2e9e06f85e01b
Sara-Toughoj <ss.toughoj@gmail.com>
2023-05-09 10:08:31 +0300

    changes

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,37 +1,40 @@
 # It was a {Adjective} and {Adjective} {Noun}.
 print(
-    '''
+    """
     ********************************
     ** Welcome to the madlib-game **
     ********************************
-    ''')
+    """
+)
+
 
 def read_template(file_path):
     with open(file_path, mode="r") as f:
-        res=f.read()
-        return (res) 
+        res = f.read()
+        return res
 
 
 def parse_template(x):
-    j=x.replace(".",'' )
-    a=j.split(" ")
-    v=[]
+    j = x.replace(".", "")
+    a = j.split(" ")
+    v = []
     for i in range(len(a)):
-        if a[i]=="{Adjective}" or a[i]=="{Noun}":
-            k=a[i][1:-1]
+        if a[i] == "{Adjective}" or a[i] == "{Noun}":
+            k = a[i][1:-1]
             v.append(k)
-            p=tuple(v)
+            p = tuple(v)
         else:
             continue
-    val1="Adjective"
-    val2="Noun"
-    h=x.replace(val1,'' ).replace(val2,'' )
-    expected_stripped=h
-    expected_parts=p
-    return expected_stripped,expected_parts
+    val1 = "Adjective"
+    val2 = "Noun"
+    h = x.replace(val1, "").replace(val2, "")
+    expected_stripped = h
+    expected_parts = p
+    return expected_stripped, expected_parts
 
-expected_stripped=parse_template("It was a {Adjective} and {Adjective} {Noun}.")[0]
-expected_parts=parse_template("It was a {Adjective} and {Adjective} {Noun}.")[1]
+
+expected_stripped = parse_template("It was a {Adjective} and {Adjective} {Noun}.")[0]
+expected_parts = parse_template("It was a {Adjective} and {Adjective} {Noun}.")[1]
 
 # print(parse_template("It was a {} and {} {}"))
 # def parse_template(input_str):
@@ -52,34 +55,25 @@ expected_parts=parse_template("It was a {Adjective} and {Adjective} {Noun}.")[1]
 #     return expected_stripped,expected_parts
 
 
-    
-def merge(x,y):
+def merge(x, y):
     return x.format(*y)
-    
+
+
 def take_values():
-    list111=[]
+    list111 = []
     for i in range(len(expected_parts)):
-        x=input(f"Please add {expected_parts[i]} :")
+        x = input(f"Please add {expected_parts[i]} :")
         list111.append(x)
     return list111
 
-print(merge(expected_stripped,take_values())) 
-# def read_template(path):
-#     try:
-#         with open(path, 'r') as file:
-#             return file.read()
-#     except FileNotFoundError:
-#         print(f"File not found: {path}")
-#         raise
+
+print(merge(expected_stripped, take_values()))
 
 
-   
-   
-   
-   
-   
-
-
-
-
-
+def read_template(path):
+    try:
+        with open(path, "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+        raise
